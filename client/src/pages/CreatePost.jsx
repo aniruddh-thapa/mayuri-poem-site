ca7665dbd1e5d4ecea0803bdca51a192caa36f83
aniruddh-thapa <thapaaniruddh@gmail.com>
2024-10-12 01:35:13 +0530

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,12 +6,15 @@ import { CircularProgressbar } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
+import {useNavigate} from 'react-router-dom'
 
 export default function CreatePost() {
     const [file, setFile] = useState(null);
     const [imageUploadProgress, setImageUploadProgress] = useState(null);
     const [imageUploadError, setImageUploadError] = useState(null);
     const [formData, setFormData] = useState({});
+    const [publishError, setPublishError] = useState(null);
+    const navigate = useNavigate();
     const handleUploadImage = async () => {
         try {
             if (!file) {
@@ -48,10 +51,34 @@ export default function CreatePost() {
             console.log(error);
           }
         };
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            try {
+              const res = await fetch('/api/post/create', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+              });
+              const data = await res.json();
+              if (!res.ok) {
+                setPublishError('Arey Mayuri poem with this title already exists yaar');
+                return;
+              }
+        
+              if (res.ok) {
+                setPublishError(null);
+                navigate(`/post/${data.slug}`);
+              }
+            } catch (error) {
+              setPublishError('Something went wrong');
+            }
+          };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold' >Create a poem</h1>
-        <form className='flex flex-col gap-4'>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
             type='text'
@@ -142,7 +169,11 @@ export default function CreatePost() {
         <Button type='submit' gradientDuoTone='redToYellow'>
             Publish
         </Button>
-    
+        {publishError && (
+          <Alert className='mt-5' color='failure'>
+            {publishError}
+          </Alert>
+        )}
         </form>
     </div>
   )
